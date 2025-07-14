3bcefd0baf8128c294c14afa3d22776b410eb1e5
EfeEdohor <edohor.efe@gmail.com>
2025-07-14 16:25:59 +0100

    Injected the Weather Search/Weather Data

--- src/index.js
+++ src/index.js
@@ -7,11 +7,10 @@ function refreshWeather(response) {
   let speedElement = document.querySelector("#speed");
   let timeElement = document.querySelector("#time");
   let date = new Date(response.data.time * 1000);
+  let iconElement = document.querySelector("#icon");
 
-  console.log(response.data);
-
+  iconElement.innerHTML = `<img src="${response.data.condition.icon_url}" class="weather-now-temperature-icon"/>`;
   cityElement.innerHTML = response.data.city;
-
   timeElement.innerHTML = formatDate(date);
   descriptionElement.innerHTML = response.data.condition.description;
   humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
